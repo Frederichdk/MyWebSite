1efc45642fe954b9c5dd79f23c922a21bd8ac092
frederichdk <fw21deko@siena.edu>
2024-07-11 20:50:51 +0200

    Stuck with div zoom on test

---
+++ Public/JavaScript/test3.js
@@ -0,0 +1,20 @@
+const zoom = document.querySelector(".zobject");
+const minZoom = 0.2; // Minimum zoom value
+const maxZoom = 1; // Starting scale value
+
+addEventListener("scroll", (e) => {
+  const vh = window.innerHeight / 100;
+  const scrollTop = document.documentElement.scrollTop;
+  const start = 100 * vh;
+  const stop = 200 * vh;
+
+  if (scrollTop > start && scrollTop < stop) {
+    const progress = (scrollTop - start) / (stop - start);
+    const scale = Math.max(maxZoom - progress * (maxZoom - minZoom), minZoom);
+    zoom.style.transform = `scale(${scale})`;
+  } else if (scrollTop <= start) {
+    zoom.style.transform = `scale(${maxZoom})`;
+  } else if (scrollTop >= stop) {
+    zoom.style.transform = `scale(${minZoom})`;
+  }
+});
